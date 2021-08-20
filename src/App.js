2898ef8fd15e0e56119ca87107c168fd13403600
abhi2322 <abhishekjamwal34@gmail.com>
2021-08-21 00:41:02 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import Home from './Home.js';
+import Header from './Header';
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

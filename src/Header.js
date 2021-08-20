d24feb7359cb76966538a5fb113432c15fd2576f
abhi2322 <abhishekjamwal34@gmail.com>
2021-08-21 00:58:06 +0530

    adding css

--- src/Header.js
+++ src/Header.js
@@ -29,7 +29,7 @@ function Header() {
                     <span className="header__optionLineTwo">Prime</span>
                 </div>
                 <div className="header__optionBasket">
-                    <ShoppingBasketIcon />
+                    <ShoppingBasketIcon/>
                     <span className="header__optionLineTwo header__basketCount">
                     0
                     </span>
